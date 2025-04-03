c7d2abadb3e94507879cdc95913dd75bd110ab8c
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2025-04-03 14:40:48 -0700

    Initial Commit

---
+++ Cookture Prototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookture Prototype
+//
+//  Created by Myung Joon Kang on 2025-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cookture Prototype/Cookture_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookture_PrototypeApp.swift
+//  Cookture Prototype
+//
+//  Created by Myung Joon Kang on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Cookture_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
